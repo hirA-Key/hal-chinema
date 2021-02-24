40e7c9a33c20e83fd2061f1930f3eb1242b57563
hirA-Key <hira12.tai11@gmail.com>
2021-02-24 17:48:09 +0900

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+//config内の値は自分のプロジェクトの設定に合わせて指定
+import env from "../.env";
+
+const firebaseConfig = {
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  messagingSenderId: env.FIREBASE_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(config);
+
